731b210d93473625d80e90279baaa65418aa09bb
memo <me@memo.blue>
2016-06-23 09:10:29 -0400

    add missing API tests

--- app/tests/api/TodoApi.test.jsx
+++ app/tests/api/TodoApi.test.jsx
@@ -1,19 +1,57 @@
 var expect = require('expect');
 
-var todoApi = require('TodoApi');
+var TodoApi = require('TodoApi');
 
 describe('TodoApi', () => {
-  beforeEach(() => {
+  beforeEach(() => { // Mocah method
     localStorage.removeItem('todos');
   });
 
   it('should exist', () => {
-    expect(todoApi).toExist();
+    expect(TodoApi).toExist();
   });
 
 
-  // TODO: test get and set
+  describe('setTodos', () => {
+    it('should set valid todos array'), () => {
+      var todos = [{
+        id: 1,
+        text: 'Sample todo text',
+        completed: false
+      }];
+      TodoApi.setTodos(todos);
+      var actualTodos = JSON.parse(localStorage.getItem('todos'));
 
+      expect(actualTodos).toEqual(todos);
+    };
+
+    it('should not set invalid todos array', () => {
+      var invalidTodos = { a: 'b' };
+      TodoApi.setTodos(invalidTodos);
+
+      expect(localStorage.getItem('todos')).toBe(null);
+    });
+  });
+
+  describe('getTodos', () => {
+    it('should return an empty array for invalid localStorage data', () => {
+      var actualTodos = TodoApi.getTodos();
+
+      expect(actualTodos).toEqual([]);
+    });
+
+    it('should return todos if valid array in localStorage', () => {
+      var todos = [{
+        id: 1,
+        text: 'Sample todo text',
+        completed: false
+      }];
+      localStorage.setItem('todos', JSON.stringify(todos));
+      var actualTodos = TodoApi.getTodos();
+
+      expect(actualTodos).toEqual(todos);
+    });
+  });
 
   describe('filterTodos', () => {
     var todos = [
@@ -35,13 +73,13 @@ describe('TodoApi', () => {
     ];
 
     it('should return all items if showCompleted is true', () => {
-      var filteredTodos = todoApi.filterTodos(todos, true, '');
+      var filteredTodos = TodoApi.filterTodos(todos, true, '');
 
       expect(filteredTodos.length).toBe(3);
     });
 
     it('should return non-completed items if showCompleted is false', () => {
-      var filteredTodos = todoApi.filterTodos(todos, false, '');
+      var filteredTodos = TodoApi.filterTodos(todos, false, '');
 
       expect(filteredTodos.length).toBe(1);
     });
