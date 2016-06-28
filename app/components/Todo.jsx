70f27b97cef1ce4cf34d4fd831b284a275864cbd
memo <me@memo.blue>
2016-06-27 21:16:07 -0400

    add missing space

--- app/components/Todo.jsx
+++ app/components/Todo.jsx
@@ -10,7 +10,7 @@ var Todo = React.createClass({
       var timestamp = createdAt;
 
       if (completed) {
-        msg = 'Completed';
+        msg = 'Completed ';
         timestamp = completedAt;
       }
 
