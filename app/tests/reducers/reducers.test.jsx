b9d9edf8ce0ae63e91ee9196d1863143d60be23e
memo <me@memo.blue>
2016-07-07 08:38:51 -0400

    fix typo

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -1,6 +1,6 @@
 var expect = require('expect');
-var df = require('deep-freeze-strict')
-;
+var df = require('deep-freeze-strict');
+
 var reducers = require('reducers');
 
 describe('Reducers', () => {
