3bc6c1d171f36c409ac4ee8a9deba0a1210b2cd7
memo <me@memo.blue>
2016-07-07 21:44:45 -0400

    add redux store

--- app/app.jsx
+++ app/app.jsx
@@ -3,6 +3,17 @@ var ReactDOM = require('react-dom');
 var { Route, Router, IndexRoute, hashHistory } = require('react-router');
 var TodoApp = require('TodoApp');
 
+var actions = require('actions');
+var store = require('configureStore').configure();
+
+store.subscribe(() => {
+  console.log('State: ', store.getState());
+});
+
+store.dispatch(actions.addTodo('Clean something for 15mn'));
+store.dispatch(actions.setSearchText('Yarlop'));
+store.dispatch(actions.toggleShowCompleted());
+
 // Load foundation
 $(document).foundation();
 
