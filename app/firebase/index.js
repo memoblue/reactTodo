3f28c5c9667a76fee2055b3c2b329cd1edce04d1
memo <me@memo.blue>
2016-07-14 07:53:28 -0400

    setup firebase, install redux-thunk, add `app` to config

---
+++ app/firebase/index.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase';
+
+try {
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB-O952KDFCq-BDPvFOk2n-fko6Q3vuy6E",
+    authDomain: "memotodo-38e5a.firebaseapp.com",
+    databaseURL: "https://memotodo-38e5a.firebaseio.com",
+    storageBucket: "memotodo-38e5a.appspot.com",
+  };
+  firebase.initializeApp(config);
+} catch (e) {
+
+}
+
+export var fbRef = firebase.database.ref();
+export default firebase; // so we can import only this file elsewhere
